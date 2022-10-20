30167f644b7784adf5b35f0f2218fde157f3dee8
zzy <1281935362@qq.com>
2022-10-20 21:43:19 +0800

    README

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
-### 代码结构
-- testcase 存放测试用例
-- business 存放业务代码
+### README
+- config：存放配置文件
+- common：存放业务代码
+- data：存放测试数据
+- testcase：存放测试用例
+- utils：存放工具类
--- run.py
+++ run.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     report_path = Conf.get_report_path()+os.sep+"result"
     report_html_path = Conf.get_report_path()+os.sep+"html"
     pytest.main(["-s","--alluredir",report_path])
-    #Base.send_mail()
+
 
 
 
